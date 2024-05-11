3e05d71695e02658339d313dafe16b2aaa0cc71e
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-05-10 23:59:15 -0400

    Initial commit

---
+++ src/main/java/com/github/tatercertified/noend/NoEnd.java
@@ -0,0 +1,13 @@
+package com.github.tatercertified.noend;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoEnd implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+    }
+    public static final String MOD_ID = "no_end";
+}
